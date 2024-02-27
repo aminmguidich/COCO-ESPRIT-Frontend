4ece872dab9d08db88d10b0a38e144f799c30fb0
hssan861 <hssan.ghorbel@esprit.tn>
2024-02-27 16:34:30 +0100

    FrontOfficeIntegration

--- COCO-ESPRIT/src/app/app-routing.module.ts
+++ COCO-ESPRIT/src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
 import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+import { AboutFrontComponent } from './FrontOffice/about-front/about-front.component';
+import { ServicesFrontComponent } from './FrontOffice/services-front/services-front.component';
+import { PircingFrontComponent } from './FrontOffice/pircing-front/pircing-front.component';
+import { CarsFrontComponent } from './FrontOffice/cars-front/cars-front.component';
+import { BlogFrontComponent } from './FrontOffice/blog-front/blog-front.component';
+import { ContactFrontComponent } from './FrontOffice/contact-front/contact-front.component';
 
 const routes: Routes = [
   {
@@ -9,11 +15,38 @@ const routes: Routes = [
     component :AllTemplateFrontComponent,
     children:[
       {
-        path:"",
+        path:"home",
         component:HomeFrontComponent
-      }
+      },
+      {
+        path :"about",
+        component:AboutFrontComponent
+      },
+      {
+        path :"services",
+        component:ServicesFrontComponent
+      },
+      {
+        path:"pircing",
+        component:PircingFrontComponent
+      },
+      {
+        path:"cars",
+        component:CarsFrontComponent
+      },
+      {
+        path:"blog",
+        component:BlogFrontComponent
+      },
+      {
+        path:"contact",
+        component:ContactFrontComponent
+      },
+
     ]
-  }
+  },
+
+
  
 ];
 
