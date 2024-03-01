32b1f22c70243c3b1b1a848468bd7e52e838da24
chayma2 <Chayma.Attafi@esprit.tn>
2024-03-01 16:11:43 +0100

    BackOffice

--- COCO-ESPRIT/src/app/BackOffice/all-template-back/all-template-back.component.ts
+++ COCO-ESPRIT/src/app/BackOffice/all-template-back/all-template-back.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-all-template-back',
-  templateUrl: './all-template-back.component.html',
-  styleUrls: ['./all-template-back.component.css']
+  templateUrl: './all-template-back.component.html'
 })
 export class AllTemplateBackComponent {
 
--- COCO-ESPRIT/src/app/app-routing.module.ts
+++ COCO-ESPRIT/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
 import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
 import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
 
@@ -13,6 +14,10 @@ const routes: Routes = [
         component:HomeFrontComponent
       }
     ]
+  },
+  {
+path :"admin",
+component :AllTemplateBackComponent
   }
  
 ];
